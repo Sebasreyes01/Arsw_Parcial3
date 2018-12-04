d54abfb154dbabbbadeb84ba07edc191d95ff352
Sebastián Reyes <sebastianreyes01@hotmail.com>
2018-12-04 11:49:08 -0500

    cuarta parte

---
+++ README.md
@@ -0,0 +1,6 @@
+# Parcial 3, Sebastián Camilo Reyes Villamil.
+## Links
+- Heroku: https://arsw-parcial3.herokuapp.com
+## Arquitectura
+La aplicación se compone de una pagina web y de un servicio API-REST.
+
--- src/test/java/com/edu/eci/arsw/parcial3/Parcial3ApplicationTests.java
+++ src/test/java/com/edu/eci/arsw/parcial3/Parcial3ApplicationTests.java
@@ -3,14 +3,20 @@ package com.edu.eci.arsw.parcial3;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class Parcial3ApplicationTests {
+    
+    @LocalServerPort
+    private int port;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        
+    }
 
 }
